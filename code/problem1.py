4ffe2809ffbe55d080fe0030fc4049901f9105a8
Jodieadver <lisijia1028111@gmail.com>
2024-04-24 20:28:39 +0200

    finish problem1

--- code/problem1.py
+++ code/problem1.py
@@ -11,9 +11,15 @@ DO NOT SHARE/DISTRIBUTE SOLUTIONS WITHOUT THE INSTRUCTOR'S PERMISSION
 word_index_dict = {}
 
 # TODO: read brown_vocab_100.txt into word_index_dict
+with open('data/brown_vocab_100.txt', 'r') as file:
+    for index, word in enumerate(file):
+            word = word.rstrip() #remove newline character
+            word_index_dict[word] = index 
+print(word_index_dict)
 
 # TODO: write word_index_dict to word_to_index_100.txt
-
+with open('word_to_index_100.txt', 'w') as wf:
+      wf.write(str(word_index_dict))
 
 
 print(word_index_dict['all'])
